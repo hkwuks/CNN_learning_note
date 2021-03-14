cf63a7c090578150928bfb96663a06c70cdb7332
hkwuks <1013302315@qq.com>
2021-03-14 19:48:14 +0800

    Update LeNet.py

--- LeNet.py
+++ LeNet.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.optim import optimizer
 import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
@@ -12,17 +13,17 @@ transform = transforms.Compose([
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='./data',train=True,download=True,transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset,batch_size=4,shuffle=True,num_workers=2)
+trainset = torchvision.datasets.CIFAR10(root='./data',train=True,download=False,transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset,batch_size=20,shuffle=True,num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root="./data",train=False,download=True,transform=transform)
-testloader = torch.utils.data.DataLoader(testset,batch_size=4,shuffle=False,num_workers=2)  # shuffle:打散
+testset = torchvision.datasets.CIFAR10(root="./data",train=False,download=False,transform=transform)
+testloader = torch.utils.data.DataLoader(testset,batch_size=100,shuffle=False,num_workers=2)  # shuffle:打散
 
-classes = ("plane", "car", "bird", "cat", "deer", "dog", "frog", "horse","ship", "truck")
+classes = ("plane", "car", "bird", "cat", "deer", "dog", "frog", "horse",
+           "ship", "truck")
 
-# functions to show an image
 
-
+# functions to show an imag
 def imshow(img):
     img = img / 2 + 0.5
     npimg = img.numpy()
@@ -55,20 +56,22 @@ def main():
 
     net = Net().cuda()
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    # optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
 
+    # 训练
     for epoch in range(10):
 
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
             # get the inputs;data is a list of [inputs,labels]
-            inputs, labels = data[0].cuda(),data[1].cuda()
+            inputs, labels = data[0].cuda(), data[1].cuda()
 
             # zero the parameter gradients
             optimizer.zero_grad()
 
             # forward + backward + optimize
-            outputs = net(inputs.cuda()).cuda()
+            outputs = net(inputs)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
@@ -79,6 +82,21 @@ def main():
                 print('[%d,%5d] loss: %.3f' %
                       (epoch + 1, i + 1, running_loss / 2000))
                 running_loss = 0.0
+
+                # 预测test样本
+                total = 0
+                correct = 0
+                for i, test_data in enumerate(testloader):
+                    with torch.no_grad():
+                        test_inputs, test_labers = test_data[0].cuda(
+                        ), test_data[1].cuda()
+                        test_outputs = net(test_inputs)
+                        predicts = torch.max(test_outputs, 1)[1]
+                    total += test_labers.size(0)
+                    correct += (predicts == test_labers).sum()
+                accuracy = correct / total * 100
+                print("Accuracy of the network on the 100 test images: {}%".
+                      format(accuracy))
 
     print('Finished Training')
 
